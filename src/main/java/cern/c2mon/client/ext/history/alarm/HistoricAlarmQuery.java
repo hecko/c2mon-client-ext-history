fdacbcc4b1563582e26996c9cf6b1405c4de0240
Franz Ritter <franz.ritter@cern.ch>
2016-08-23 09:53:58 +0200

    Convert timestamps into utc time before sending them to the database.
    Issue: #1

--- src/main/java/cern/c2mon/client/ext/history/alarm/HistoricAlarmQuery.java
+++ src/main/java/cern/c2mon/client/ext/history/alarm/HistoricAlarmQuery.java
@@ -1,21 +1,22 @@
 /******************************************************************************
  * Copyright (C) 2010-2016 CERN. All rights not expressly granted are reserved.
- * 
+ *
  * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
  * C2MON is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the license.
- * 
+ *
  * C2MON is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
 package cern.c2mon.client.ext.history.alarm;
 
+import cern.c2mon.client.ext.history.dbaccess.util.TimeZoneUtil;
 import com.mysema.query.BooleanBuilder;
 import com.mysema.query.types.Predicate;
 
@@ -57,7 +58,8 @@ public class HistoricAlarmQuery {
   }
 
   public HistoricAlarmQuery timestamp(Timestamp timestamp) {
-    builder.and(QAlarm.alarm.timestamp.eq(timestamp));
+    Timestamp utcTimeStamp = TimeZoneUtil.convertDateTimezoneToUtc(timestamp);
+    builder.and(QAlarm.alarm.timestamp.eq(utcTimeStamp));
     return this;
   }
 
@@ -72,7 +74,9 @@ public class HistoricAlarmQuery {
   }
 
   public HistoricAlarmQuery between(Timestamp start, Timestamp end) {
-    builder.and(QAlarm.alarm.timestamp.between(start, end));
+    Timestamp utcStart = TimeZoneUtil.convertDateTimezoneToUtc(start);
+    Timestamp utcEnd = TimeZoneUtil.convertDateTimezoneToUtc(end);
+    builder.and(QAlarm.alarm.timestamp.between(utcStart, utcEnd));
     return this;
   }
 
--- src/main/java/cern/c2mon/client/ext/history/dbaccess/util/TimeZoneUtil.java
+++ src/main/java/cern/c2mon/client/ext/history/dbaccess/util/TimeZoneUtil.java
@@ -1,16 +1,16 @@
 /******************************************************************************
  * Copyright (C) 2010-2016 CERN. All rights not expressly granted are reserved.
- * 
+ *
  * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
  * C2MON is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the license.
- * 
+ *
  * C2MON is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
@@ -24,15 +24,15 @@ import java.util.TimeZone;
 
 /**
  * Methods for managing time zones
- * 
+ *
  * @author vdeila
- * 
+ *
  */
 public final class TimeZoneUtil {
 
   /**
    * Converts a date into another timezone
-   * 
+   *
    * @param timeZone
    *          The timezone to convert into
    * @param date
@@ -53,7 +53,7 @@ public final class TimeZoneUtil {
   }
 
   /**
-   * 
+   *
    * @param date
    *          the date which is in UTC timezone
    * @return the date in the local time zone
@@ -66,7 +66,7 @@ public final class TimeZoneUtil {
 
   /**
    * Converts a date from one timezone to another
-   * 
+   *
    * @param newTimezone
    *          the new timezone that is wanted for the date
    * @param date
@@ -77,11 +77,22 @@ public final class TimeZoneUtil {
    * @return the converted date
    */
   public static Timestamp convertDateTimezone(final TimeZone newTimezone, final Date date, final TimeZone dateTimezone) {
-    long newTime = 
-      date.getTime() 
-      - dateTimezone.getOffset(date.getTime()) 
+    long newTime =
+      date.getTime()
+      - dateTimezone.getOffset(date.getTime())
       + newTimezone.getOffset(date.getTime());
     return new Timestamp(newTime);
+  }
+
+  /**
+   * Converts a date to the utc timezone
+   * @param date
+   *          the date is will be converted. Assumed to be in the
+   *          <code>dateTimezone</code>
+   * @return the converted date
+   */
+  public static Timestamp convertDateTimezoneToUtc(final Date date) {
+    return convertDateTimezone(TimeZone.getTimeZone("UTC"), date, TimeZone.getDefault());
   }
 
   /**
