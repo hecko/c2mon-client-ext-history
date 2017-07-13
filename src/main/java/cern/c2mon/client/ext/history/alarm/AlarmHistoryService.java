3e161c36abbc0035b3a75a8bef73e13cc8b1fafa
Matthias Braeger <Matthias.Braeger@cern.ch>
2017-07-13 12:09:28 +0200

    #7: Use Pageable instead of PageRequest in interface

--- src/main/java/cern/c2mon/client/ext/history/alarm/AlarmHistoryService.java
+++ src/main/java/cern/c2mon/client/ext/history/alarm/AlarmHistoryService.java
@@ -1,25 +1,25 @@
 /******************************************************************************
  * Copyright (C) 2010-2016 CERN. All rights not expressly granted are reserved.
- * 
+ *
  * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
  * C2MON is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the license.
- * 
+ *
  * C2MON is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
 package cern.c2mon.client.ext.history.alarm;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-
 import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 /**
  * This service allows querying {@link Alarm} history from the c2mon history database.
@@ -50,5 +50,5 @@ public interface AlarmHistoryService {
    *
    * @return a page of matched results
    */
-  Page<Alarm> findBy(HistoricAlarmQuery query, PageRequest page);
+  Page<Alarm> findBy(HistoricAlarmQuery query, Pageable page);
 }
--- src/main/java/cern/c2mon/client/ext/history/alarm/impl/AlarmHistoryServiceImpl.java
+++ src/main/java/cern/c2mon/client/ext/history/alarm/impl/AlarmHistoryServiceImpl.java
@@ -1,35 +1,35 @@
 /******************************************************************************
  * Copyright (C) 2010-2016 CERN. All rights not expressly granted are reserved.
- * 
+ *
  * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
  * C2MON is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the license.
- * 
+ *
  * C2MON is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
 package cern.c2mon.client.ext.history.alarm.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TimeZone;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import cern.c2mon.client.ext.history.alarm.Alarm;
 import cern.c2mon.client.ext.history.alarm.AlarmHistoryService;
 import cern.c2mon.client.ext.history.alarm.HistoricAlarmQuery;
 import cern.c2mon.client.ext.history.alarm.repository.AlarmRepository;
 import cern.c2mon.client.ext.history.dbaccess.util.TimeZoneUtil;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.TimeZone;
 
 /**
  * @author Justin Lewis Salmon
@@ -46,7 +46,7 @@ public class AlarmHistoryServiceImpl implements AlarmHistoryService {
   }
 
   @Override
-  public Page<Alarm> findBy(HistoricAlarmQuery query, PageRequest page) {
+  public Page<Alarm> findBy(HistoricAlarmQuery query, Pageable page) {
     return (Page<Alarm>) localise(alarmRepository.findAll(query.getPredicate(), page));
   }
 
