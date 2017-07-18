2c546ac5ca4047f23aea450a3f1ee2f5fad89047
Matthias Braeger <Matthias.Braeger@cern.ch>
2017-07-18 18:23:02 +0200

    #6: Add class to convert between LocalDateTime and java.sql.timestamp

---
+++ src/main/java/cern/c2mon/client/ext/history/util/LocalDateTimeConverter.java
@@ -0,0 +1,38 @@
+/******************************************************************************
+ * Copyright (C) 2010-2017 CERN. All rights not expressly granted are reserved.
+ * <p/>
+ * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
+ * C2MON is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the license.
+ * <p/>
+ * C2MON is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+ * more details.
+ * <p/>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+package cern.c2mon.client.ext.history.util;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+
+/**
+ * Util class to convert from/to UTC time
+ *
+ * @author Matthias Braeger
+ */
+public final class LocalDateTimeConverter {
+  public static final LocalDateTime convertToLocalDateTime(Timestamp sqlDate) {
+    return ZonedDateTime.ofInstant(sqlDate.toInstant(), ZoneId.systemDefault()).toLocalDateTime();
+  }
+
+  public static final Timestamp convertToTimestamp(LocalDateTime localDateTime) {
+    return new Timestamp(localDateTime.toInstant(ZoneOffset.of(ZoneId.systemDefault().getId())).toEpochMilli());
+  }
+}
