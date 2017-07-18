f8ad9f3aad14708f0c461fbdab0fced8d0cec5f1
Matthias Braeger <Matthias.Braeger@cern.ch>
2017-07-18 18:26:12 +0200

    #6: Set hibernate.jdbc.time_zone=UTC to correctly convert SQL timestamp

--- src/main/java/cern/c2mon/client/ext/history/alarm/config/JpaConfiguration.java
+++ src/main/java/cern/c2mon/client/ext/history/alarm/config/JpaConfiguration.java
@@ -1,22 +1,26 @@
 /******************************************************************************
  * Copyright (C) 2010-2016 CERN. All rights not expressly granted are reserved.
- * 
+ *
  * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
  * C2MON is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the license.
- * 
+ *
  * C2MON is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
 package cern.c2mon.client.ext.history.alarm.config;
 
-import cern.c2mon.client.ext.history.alarm.repository.AlarmRepository;
+import java.util.Hashtable;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -26,12 +30,11 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
-import javax.sql.DataSource;
-import java.util.Collections;
+import cern.c2mon.client.core.service.AlarmService;
 
 /**
  * This class is responsible for configuring the {@link javax.persistence.EntityManager} and {@link javax.transaction.TransactionManager} instances that will
- * be used under the hood by our {@link AlarmRepository}.
+ * be used under the hood by our {@link AlarmService}.
  *
  * @author Justin Lewis Salmon
  */
@@ -54,7 +57,13 @@ public class JpaConfiguration {
     factory.setDataSource(dataSource);
     factory.setPackagesToScan("cern.c2mon.client.ext.history");
     factory.setJpaVendorAdapter(adapter);
-    factory.setJpaPropertyMap(Collections.singletonMap("javax.persistence.validation.mode", "none"));
+
+    Map<String, String> propertyMap = new Hashtable<>();
+    propertyMap.put("javax.persistence.validation.mode", "none");
+    // If not set, the alarm timestamp does not get correctly converted into the local time zone and vice versa
+    propertyMap.put("hibernate.jdbc.time_zone", "UTC");
+
+    factory.setJpaPropertyMap(propertyMap);
     return factory;
   }
 
