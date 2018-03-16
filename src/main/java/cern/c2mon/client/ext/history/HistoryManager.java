92318ed866e62349a42ff590febea78de459c110
Matthias Braeger <Matthias.Braeger@cern.ch>
2018-03-16 11:54:48 +0100

    Fix problem with notifying listeners registered on tags

--- src/main/java/cern/c2mon/client/ext/history/HistoryManager.java
+++ src/main/java/cern/c2mon/client/ext/history/HistoryManager.java
@@ -1,16 +1,16 @@
 /******************************************************************************
  * Copyright (C) 2010-2016 CERN. All rights not expressly granted are reserved.
- * 
+ *
  * This file is part of the CERN Control and Monitoring Platform 'C2MON'.
  * C2MON is free software: you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation, either version 3 of the license.
- * 
+ *
  * C2MON is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with C2MON. If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
@@ -20,8 +20,7 @@ import java.util.*;
 
 import javax.annotation.PostConstruct;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -31,11 +30,9 @@ import cern.c2mon.client.core.cache.BasicCacheHandler;
 import cern.c2mon.client.core.jms.ConnectionListener;
 import cern.c2mon.client.core.jms.SupervisionListener;
 import cern.c2mon.client.core.listener.TagSubscriptionListener;
-import cern.c2mon.client.core.listener.TagUpdateListener;
 import cern.c2mon.client.core.service.AdvancedTagService;
 import cern.c2mon.client.core.service.CoreSupervisionService;
 import cern.c2mon.client.core.tag.TagController;
-import cern.c2mon.client.core.tag.TagImpl;
 import cern.c2mon.client.ext.history.common.*;
 import cern.c2mon.client.ext.history.common.exception.HistoryPlayerNotActiveException;
 import cern.c2mon.client.ext.history.common.tag.HistoryTagManager;
@@ -47,43 +44,37 @@ import cern.c2mon.client.ext.history.util.KeyForValuesMap;
 import cern.c2mon.shared.common.supervision.SupervisionConstants.SupervisionEntity;
 
 @Service
+@Slf4j
 public class HistoryManager implements C2monHistoryManager, TagSubscriptionListener {
 
-  /** Log4j logger for this class */
-  private static final Logger LOG = LoggerFactory.getLogger(HistoryManager.class);
-  
   /** Reference to the <code>TagManager</code> singleton */
   private final AdvancedTagService tagService;
 
   /** Reference to the <code>ClientDataTagCache</code> */
   private final BasicCacheHandler cache;
-  
+
   /** Reference to the <code>C2monSupervisionManager</code> */
   private final CoreSupervisionService coreSupervisionService;
-  
+
   /** Reference to the {@link HistoryTagManager} */
   private final HistoryTagManager historyTagManager;
-  
+
   /** the history player */
   private HistoryPlayerCoreAccess historyPlayer = null;
-  
+
   /** Keeps track of which tag id belongs to which {@link SupervisionListener} */
-  private final KeyForValuesMap<Long, SupervisionListener> tagToSupervisionListener 
-    = new KeyForValuesMap<Long, SupervisionListener>();
-  
-  /** Interface to keep track of which providers are available */
-  private HistoryProviderAvailability historyProviderAvailability = null;
-  
+  private final KeyForValuesMap<Long, SupervisionListener> tagToSupervisionListener = new KeyForValuesMap<>();
+
   /** A connection listener checking if the connection to the JMS is lost. */
   private ConnectionListener jmsConnectionListener = null;
 
   private HistorySessionFactory historySessionFactory;
- 
+
   @Autowired
   protected HistoryManager(final AdvancedTagService tagService, final BasicCacheHandler pCache,
       final CoreSupervisionService supervisionService, final HistoryTagManager historyTagManager,
                            @Qualifier("historyFactory") HistorySessionFactory historySessionFactory) {
-    
+
     this.tagService = tagService;
     this.cache = pCache;
     this.coreSupervisionService = supervisionService;
@@ -97,7 +88,7 @@ public class HistoryManager implements C2monHistoryManager, TagSubscriptionListe
   @SuppressWarnings("unused")
   @PostConstruct
   private void init() {
-    ((AdvancedTagService) tagService).addTagSubscriptionListener(this);
+    tagService.addTagSubscriptionListener(this);
   }
 
   @Override
@@ -106,16 +97,16 @@ public class HistoryManager implements C2monHistoryManager, TagSubscriptionListe
       throw new RuntimeException("Cannot go into history mode," +
       		" because the connection to the server is down.");
     }
-    
+
     cache.setHistoryMode(true);
-    
+
     synchronized (cache.getHistoryModeSyncLock()) {
       if (cache.isHistoryModeEnabled()) {
         if (jmsConnectionListener == null) {
           jmsConnectionListener = new ConnectionEvents();
           coreSupervisionService.addConnectionListener(jmsConnectionListener);
         }
-        
+
         if (historyPlayer == null) {
           historyPlayer = new HistoryPlayerImpl();
         }
@@ -128,7 +119,7 @@ public class HistoryManager implements C2monHistoryManager, TagSubscriptionListe
         try {
 
           // Subscribes all the current subscribed tags
-          subscribeTagsToHistory(cache.getAllSubscribedDataTags());
+          subscribeTagsToHistory(cache.getAllTagControllers());
         }
         catch (RuntimeException e) {
           historyPlayer.deactivateHistoryPlayer();
@@ -145,7 +136,7 @@ public class HistoryManager implements C2monHistoryManager, TagSubscriptionListe
       if (historyPlayer != null) {
         historyPlayer.stopLoading();
       }
-      
+
       synchronized (cache.getHistoryModeSyncLock()) {
         if (historyPlayer != null) {
           historyPlayer.deactivateHistoryPlayer();
@@ -160,7 +151,7 @@ public class HistoryManager implements C2monHistoryManager, TagSubscriptionListe
     if (tagIds != null && tagIds.size() > 0) {
       synchronized (cache.getHistoryModeSyncLock()) {
         if (cache.isHistoryModeEnabled()
-            && this.historyPlayer != null 
+            && this.historyPlayer != null
             && this.historyPlayer.isHistoryPlayerActive()) {
           subscribeTagsToHistory(tagIds);
         }
@@ -180,12 +171,12 @@ public class HistoryManager implements C2monHistoryManager, TagSubscriptionListe
    * <br/>
    * The caller must hold the {@link BasicCacheHandler#getHistoryModeSyncLock()}
    * when calling this method
-   * 
+   *
    * @param tagIds
    *          The tags to subscribes
    */
   private void subscribeTagsToHistory(final Set<Long> tagIds) {
-    subscribeTagsToHistory(cache.get(tagIds).values());
+    subscribeTagsToHistory(cache.getTagControllers(tagIds));
   }
 
   /**
@@ -193,57 +184,49 @@ public class HistoryManager implements C2monHistoryManager, TagSubscriptionListe
    * <br/>
    * The caller must hold the {@link BasicCacheHandler#getHistoryModeSyncLock()}
    * when calling this method
-   * 
-   * @param clientDataTags
+   *
+   * @param tagController
    *          The tags to subscribes
    */
-  private void subscribeTagsToHistory(final Collection<Tag> clientDataTags) {
+  private void subscribeTagsToHistory(final Collection<TagController> tagControllers) {
     if (cache.isHistoryModeEnabled()
-        && this.historyPlayer != null 
+        && this.historyPlayer != null
         && this.historyPlayer.isHistoryPlayerActive()) {
-      
-      // Registers all the tag update listeners and supervision listeners  
-      for (final Tag tag : clientDataTags) {
-        
-        Tag realtimeValue;
-        TagController tagController = new TagController((TagImpl) tag);
-        realtimeValue = tagController.getTagImpl().clone();
-        
-        if (realtimeValue != null) {
-          if (realtimeValue.getServerTimestamp().getTime() == 0) {
-            realtimeValue = null;
-          }
-        }
-        
+
+      // Registers all the tag update listeners and supervision listeners
+      for (final TagController tagController : tagControllers) {
+
+        Tag realtimeValue = tagController.getTagImpl().clone();
+
         tagController.clean();
-        
+
         // Registers to tag updates
         this.historyPlayer.registerTagUpdateListener(
-            (TagUpdateListener) tagController,
-            tag.getId(),
+            tagController,
+            realtimeValue.getId(),
             realtimeValue);
-        
+
         // Tracks the listener, used when for later when unsubscribing
-        tagToSupervisionListener.add(tag.getId(), (SupervisionListener) tagController);
-        
+        tagToSupervisionListener.add(realtimeValue.getId(), tagController);
+
         // Register to supervision events
         this.historyPlayer.registerSupervisionListener(
             SupervisionEntity.PROCESS,
-            (SupervisionListener) tagController,
-            tag.getProcessIds());
-        
+            tagController,
+            realtimeValue.getProcessIds());
+
         this.historyPlayer.registerSupervisionListener(
             SupervisionEntity.EQUIPMENT,
-            (SupervisionListener) tagController,
-            tag.getEquipmentIds());
-        
+            tagController,
+            realtimeValue.getEquipmentIds());
+
 //          For the day the SupervisionEntity.SUBEQUIPMENT also comes
 //          this.historyPlayer.registerSupervisionListener(
 //              SupervisionEntity.SUBEQUIPMENT,
 //              (SupervisionListener) clientDataTag,
 //              clientDataTag.getSubEquipmentIds());
       }
-      
+
       new Thread("History-Player-Begin-Loading-Thread") {
         @Override
         public void run() {
@@ -256,16 +239,16 @@ public class HistoryManager implements C2monHistoryManager, TagSubscriptionListe
 
   /**
    * Unsubscribes the tags to the history player
-   * 
+   *
    * @param tagIds
    *          The tags to unsubscribe
    */
   private void unsubscribeTagsFromHistory(final Set<Long> tagIds) {
     synchronized (cache.getHistoryModeSyncLock()) {
       if (cache.isHistoryModeEnabled()
-          && this.historyPlayer != null 
+          && this.historyPlayer != null
           && this.historyPlayer.isHistoryPlayerActive()) {
-        
+
         this.historyPlayer.unregisterTags(tagIds);
 
         // Unregistering the supervision listeners
@@ -279,7 +262,7 @@ public class HistoryManager implements C2monHistoryManager, TagSubscriptionListe
         }
       }
     }
-    
+
   }
 
   @Override
@@ -291,12 +274,12 @@ public class HistoryManager implements C2monHistoryManager, TagSubscriptionListe
       throw new HistoryPlayerNotActiveException("The history player is not active, and can therefore not be retrieved");
     }
   }
-  
+
   @Override
   public HistoryPlayerEvents getHistoryPlayerEvents() {
     synchronized (cache.getHistoryModeSyncLock()) {
       if (this.historyPlayer == null) {
-        this.historyPlayer = new HistoryPlayerImpl(); 
+        this.historyPlayer = new HistoryPlayerImpl();
       }
       return this.historyPlayer;
     }
@@ -315,14 +298,14 @@ public class HistoryManager implements C2monHistoryManager, TagSubscriptionListe
   class ConnectionEvents implements ConnectionListener {
     @Override
     public void onConnection() {
-      
+
     }
 
     @Override
     public void onDisconnection() {
       if (isHistoryModeEnabled()) {
         stopHistoryPlayerMode();
-        LOG.info("The history mode is stopped, because the connection to the server is lost.");
+        log.info("The history mode is stopped, because the connection to the server is lost.");
       }
     }
   }
@@ -331,10 +314,10 @@ public class HistoryManager implements C2monHistoryManager, TagSubscriptionListe
   public HistoryLoadingManager createHistoryLoadingManager(final HistoryProvider historyProvider, final Collection<Long> tagIds) {
     final HistoryLoadingManager manager = new HistoryLoadingManagerImpl(historyProvider);
     final Collection<Tag> cdtValues = this.tagService.get(tagIds);
-    final List<Tag> cdts = new ArrayList<Tag>();
+    final List<Tag> cdts = new ArrayList<>();
     for (final Tag cdtValue : cdtValues) {
       if (cdtValue instanceof Tag) {
-        cdts.add((Tag) cdtValue);
+        cdts.add(cdtValue);
       }
       else {
         throw new RuntimeException(String.format("The '%s' must be of type '%s'", Tag.class.getName(), Tag.class.getName()));
@@ -352,7 +335,7 @@ public class HistoryManager implements C2monHistoryManager, TagSubscriptionListe
       if (tagValues == null || tagValues.size() == 0) {
         throw new RuntimeException("Cannot get the client data tag value for the tag id " + tagId);
       }
-      return (Tag) tagValues.iterator().next();
+      return tagValues.iterator().next();
     }
   }
 
@@ -360,5 +343,5 @@ public class HistoryManager implements C2monHistoryManager, TagSubscriptionListe
   public HistoryTagManager getHistoryTagManager() {
     return this.historyTagManager;
   }
-  
+
 }
